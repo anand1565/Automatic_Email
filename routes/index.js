f04182ab25eb8511e89c9c91f91fa57fe61e9854
anand1565 <113083011+anand1565@users.noreply.github.com>
2024-02-11 11:54:31 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,35 +8,8 @@ const defaultClient = SibApiV3Sdk.ApiClient.instance;
 var apiKey = defaultClient.authentications['api-key'];
 apiKey.apiKey = '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';
 
-
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  // res.render('index', { title: 'Express' });
-  res.send("hello");
-});
-
-router.get('/submit', function (req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.post('/submit1', async (req, res) => {
-  const { name, email } = req.body;
-
-  // Send email using Brevo API
-  // const brevo = new SibApiV3Sdk("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");
-  Brevo.send({
-    to: email,
-    subject: 'Thanks for Subscribing',
-    text: `Thanks for Subscribing ${name}`
-  })
-    .then(() => {
-      console.log('Email sent successfully');
-      res.sendStatus(200);
-    })
-    .catch(error => {
-      console.error('Error sending email:', error);
-      res.sendStatus(500);
-    });
 });
 
 router.post('/submit', async (req, res) => {
